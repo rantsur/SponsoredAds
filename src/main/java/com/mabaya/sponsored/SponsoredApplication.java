2dbb02b0995beac4c2c078589ab1e43131a1a4b7
rant <ratsur7@gmail.com>
2020-06-08 18:42:00 +0300

    Initial commit

---
+++ src/main/java/com/mabaya/sponsored/SponsoredApplication.java
@@ -0,0 +1,15 @@
+package com.mabaya.sponsored;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SponsoredApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(SponsoredApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mabaya/sponsored/SponsoredApplicationTests.java
@@ -0,0 +1,15 @@
+package com.mabaya.sponsored;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SponsoredApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
