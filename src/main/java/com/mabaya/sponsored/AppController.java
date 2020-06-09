82596050c813f66a39ccad347afed767bc9f1a40
rant <ratsur7@gmail.com>
2020-06-09 11:21:52 +0300

    Build API entry points

--- src/main/java/com/mabaya/sponsored/AppController.java
+++ src/main/java/com/mabaya/sponsored/AppController.java
@@ -1,13 +1,20 @@
 package com.mabaya.sponsored;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AppController
 {
-	@RequestMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot!";
+	@ResponseBody @RequestMapping("/newCampaign")
+	public Campaign createCampaign(@RequestBody Campaign campaign){
+		return new Campaign(campaign.getName(), campaign.bid, campaign.sellerId);
+	}
+
+	@ResponseBody @RequestMapping("/getProduct")
+	public Product getPromotedProduct(@RequestBody String category){
+		return null;
 	}
 }
