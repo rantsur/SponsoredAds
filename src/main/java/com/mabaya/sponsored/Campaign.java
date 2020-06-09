43ba67e5d938cc957402892b5e3e237a514d3dd3
rant <ratsur7@gmail.com>
2020-06-09 10:41:27 +0300

    Create Campaign and Product entities

---
+++ src/main/java/com/mabaya/sponsored/Campaign.java
@@ -0,0 +1,102 @@
+package com.mabaya.sponsored;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Created by Ran Tsur on 09-Jun-20.
+ */
+public class Campaign
+{
+	public enum Status{
+		ACTIVE, DELETED
+	}
+
+	String name;
+	Status status;
+	BigDecimal bid;
+	String sellerId;
+	List<Product> productList;
+
+	public Campaign(String name, BigDecimal bid, String sellerId)
+	{
+		this.name = name;
+		this.bid = bid;
+		this.sellerId = sellerId;
+		this.status = Status.ACTIVE;
+		this.productList = new ArrayList<>();
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public Status getStatus()
+	{
+		return status;
+	}
+
+	public void setStatus(Status status)
+	{
+		this.status = status;
+	}
+
+	public BigDecimal getBid()
+	{
+		return bid;
+	}
+
+	public void setBid(BigDecimal bid)
+	{
+		this.bid = bid;
+	}
+
+	public String getSellerId()
+	{
+		return sellerId;
+	}
+
+	public void setSellerId(String sellerId)
+	{
+		this.sellerId = sellerId;
+	}
+
+	public List<Product> getProductList()
+	{
+		return productList;
+	}
+
+	public void setProductList(List<Product> productList)
+	{
+		this.productList = productList;
+	}
+
+	@Override
+	public boolean equals(Object o)
+	{
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		Campaign campaign = (Campaign) o;
+		return Objects.equals(name, campaign.name) && status == campaign.status && Objects.equals(bid, campaign.bid) && Objects.equals(sellerId, campaign.sellerId) && Objects
+				.equals(productList, campaign.productList);
+	}
+
+	@Override
+	public int hashCode()
+	{
+		return Objects.hash(name, status, bid, sellerId, productList);
+	}
+}
---
+++ src/main/java/com/mabaya/sponsored/Product.java
@@ -0,0 +1,95 @@
+package com.mabaya.sponsored;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+/**
+ * Created by Ran Tsur on 09-Jun-20.
+ */
+public class Product
+{
+	String title;
+	BigDecimal price;
+	String category;
+	String serial;
+	String sellerId;
+
+	public Product(String title, BigDecimal price, String category, String serial, String sellerId)
+	{
+		this.title = title;
+		this.price = price;
+		this.category = category;
+		this.serial = serial;
+		this.sellerId = sellerId;
+	}
+
+	public String getTitle()
+	{
+		return title;
+	}
+
+	public void setTitle(String title)
+	{
+		this.title = title;
+	}
+
+	public BigDecimal getPrice()
+	{
+		return price;
+	}
+
+	public void setPrice(BigDecimal price)
+	{
+		this.price = price;
+	}
+
+	public String getCategory()
+	{
+		return category;
+	}
+
+	public void setCategory(String category)
+	{
+		this.category = category;
+	}
+
+	public String getSerial()
+	{
+		return serial;
+	}
+
+	public void setSerial(String serial)
+	{
+		this.serial = serial;
+	}
+
+	public String getSellerId()
+	{
+		return sellerId;
+	}
+
+	public void setSellerId(String sellerId)
+	{
+		this.sellerId = sellerId;
+	}
+
+	@Override
+	public boolean equals(Object o)
+	{
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		Product product = (Product) o;
+		return Objects.equals(title, product.title) && Objects.equals(price, product.price) && Objects.equals(category, product.category) && Objects.equals(serial, product.serial)
+				&& Objects.equals(sellerId, product.sellerId);
+	}
+
+	@Override
+	public int hashCode()
+	{
+		return Objects.hash(title, price, category, serial, sellerId);
+	}
+}
