d68cb94e3b3af1ae8743110555b40198d823b23a
rant <ratsur7@gmail.com>
2020-06-09 13:12:54 +0300

    Create Category entity

---
+++ src/main/java/com/mabaya/sponsored/Category.java
@@ -0,0 +1,19 @@
+package com.mabaya.sponsored;
+
+/**
+ * Created by Ran Tsur on 09-Jun-20.
+ */
+public class Category
+{
+	String name;
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+}
